110828d4a8db15df0f21071a1c3d2e9624cd5b33
AmrShakhshir <amrshakhshir@hotmail.com>
2020-06-24 20:59:13 +0200

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Landing from './Landing'
+import Post from './Post'
+
+
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Landing />
+            </div>
+        )
+    }
+}
